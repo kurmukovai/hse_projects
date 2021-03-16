089bf94c89ee7219f02fdcaca31ad0da53021e24
AlisaKugusheva <74014280+AlisaKugusheva@users.noreply.github.com>
2021-03-16 12:45:49 +0300

    Update train_step.py

--- Alice/train_step.py
+++ Alice/train_step.py
@@ -1,3 +1,5 @@
+import torch
+
 def train_step(images, labels, architecture, criterion, optimizer):
     device = ("cuda" if torch.cuda.is_available() else 'cpu')
     images, labels = images.to(device), labels.to(device)
