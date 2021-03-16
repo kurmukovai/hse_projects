fc8b8d21879f3a643ad20a5c4dcc3cb35c15638e
AlisaKugusheva <74014280+AlisaKugusheva@users.noreply.github.com>
2021-03-16 12:36:20 +0300

    Create train_step.py

---
+++ Alice/train_step.py
@@ -0,0 +1,14 @@
+def train_step(images, labels, architecture, criterion, optimizer):
+    device = ("cuda" if torch.cuda.is_available() else 'cpu')
+    images, labels = images.to(device), labels.to(device)
+    architecture.train() # enforce training regime
+    
+    pred_logits = architecture(images)
+    loss = criterion(pred_logits, labels)
+    
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    
+    return loss.data.cpu().numpy()
